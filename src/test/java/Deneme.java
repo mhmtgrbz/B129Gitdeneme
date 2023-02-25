52171bc653fc0da390dd725c5941c7ef0efd98b0
Mehmet Gurbuz <email_adresiniz>
2023-02-25 19:28:27 +0300

    v4

--- src/test/java/Deneme.java
+++ src/test/java/Deneme.java
@@ -3,7 +3,8 @@ public class Deneme {
     public static void main(String[] args) {
 
         System.out.println("merhaba");
-
+        System.out.println("dunya");
+        System.out.println("B129");
 
 
     }
